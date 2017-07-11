ebcb7866ad7cda420b3d989fcc0ebc3e85f12ef2
Ekta Chhatbar <Ekta.Chhatbar@Xoriant.com>
2017-07-11 11:00:34 +0530

    Commit

--- JS Assignments/10_sort.js
+++ JS Assignments/10_sort.js
@@ -1,11 +1,28 @@
 function sorting(){
 	var choice= document.getElementById("sort").value;
-	var num=[1,5,2,4,3];
+	var num=[1,55,2,4,33];
 	var i,j,temp;
 	if(choice=="ascending"){
-		document.write("ascending ",num.sort());
+		for(i=0;i<num.length;i++){
+			for(j=i+1;j<num.length;j++){
+				if(num[i]>num[j]){
+					temp=num[i];
+					num[i]=num[j];
+					num[j]=temp;
+				}
+			}
+		}
+		document.write("ascending ",num);
 	}else{
-		num=num.sort();
-		document.write("descending ",num.reverse());
+		for(i=0;i<num.length;i++){
+			for(j=i+1;j<num.length;j++){
+				if(num[i]<num[j]){
+					temp=num[i];
+					num[i]=num[j];
+					num[j]=temp;
+				}
+			}
+		}
+		document.write("descending ",num);
 	}
 }
